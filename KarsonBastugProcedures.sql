d08b2c63765bf43f5fdd2d3f95101a15c536cba9
Karson Bastug <91923098+karsonbastug@users.noreply.github.com>
2024-02-22 15:35:07 -0500

    Update KarsonBastugProcedures.sql

--- KarsonBastugProcedures.sql
+++ KarsonBastugProcedures.sql
@@ -1,25 +1,25 @@
+
 CREATE PROCEDURE GetRatingsForHighTemp
     @MaxTemp INT
 AS
 BEGIN
-    SELECT WH.Rating
-    FROM WeatherHealth WH
-    JOIN LocationWeather LW ON WH.LID = LW.LID AND WH.WID = LW.WID
+    SELECT HR.Rating
+    FROM HealthReview HR
+    JOIN LocationWeather LW ON HR.LID = LW.LID AND HR.WID = LW.WID
     WHERE LW.MaxTemp > @MaxTemp;
 END;
 
-EXEC GetRatingsForHighTemp @MaxTemp = 80;
+--EXEC GetRatingsForHighTemp @MaxTemp = 80;
 
 CREATE PROCEDURE GetCitiesByRating
     @Rating INT
 AS
 BEGIN
-    SELECT L.City, LW.MaxTemp, LW.MinTemp, LW.AvgTemp, LW.Precipitation
-    FROM Location L
+    SELECT L.City, LW.MaxTemp, LW.MinTemp, LW.AvgTemp, LW.Precipitation, LW.[Date]
+    FROM [Location] L
     JOIN LocationWeather LW ON L.LID = LW.LID
-    JOIN WeatherHealth WH ON LW.LID = WH.LID AND LW.WID = WH.WID
-    WHERE WH.Rating > @Rating;
+    JOIN HealthReview HR ON LW.LID = HR.LID AND LW.WID = HR.WID
+    WHERE HR.Rating > @Rating;
 END;
 
-EXEC GetCitiesByRating @Rating = 2;
-
+--EXEC GetCitiesByRating @Rating = 2;
