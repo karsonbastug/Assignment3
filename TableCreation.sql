9a3fb58828dfa3d3010fda19c1eb0ffb81aa8746
Karson Bastug <91923098+karsonbastug@users.noreply.github.com>
2024-02-22 15:18:12 -0500

    Update TableCreation.sql

--- TableCreation.sql
+++ TableCreation.sql
@@ -1,14 +1,14 @@
 CREATE TABLE [User] (
-    UID INT PRIMARY KEY NOT NULL,
+    [UID] INT PRIMARY KEY NOT NULL,
     Username VARCHAR(50) NOT NULL,
-    Password VARCHAR(50) NOT NULL,
+    [Password] VARCHAR(50) NOT NULL,
     Email VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Location (
     LID INT PRIMARY KEY,
     City VARCHAR(100) NOT NULL,
-    State VARCHAR(20) NOT NULL,
+    [State] VARCHAR(20) NOT NULL,
     ZipCode VARCHAR(20) NOT NULL
 );
 
@@ -19,18 +19,18 @@ CREATE TABLE LocationWeather (
     MinTemp INT,
     AvgTemp INT,
     Precipitation INT,
-    Date DATE,
+    [Date] DATE,
     PRIMARY KEY (LID, WID),
     FOREIGN KEY (LID) REFERENCES Location(LID)
 );
 
 CREATE TABLE HealthReview (
-    UID INT NOT NULL,
+    [UID] INT NOT NULL,
     LID INT NOT NULL,
     WID INT NOT NULL,
     Rating INT,
     Comment TEXT,
-    Date DATE,
+    [Date] DATE,
     PRIMARY KEY (UID, LID, WID),
     FOREIGN KEY (UID) REFERENCES [User](UID),
     FOREIGN KEY (LID, WID) REFERENCES LocationWeather(LID, WID)
