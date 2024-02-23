f5f2e352218300c746e192be74f299bdb2adc7f3
noahstalnaker <142910852+noahstalnaker@users.noreply.github.com>
2024-02-23 12:48:59 -0500

    Update NoahStalnakerSps.sql

--- NoahStalnakerSps.sql
+++ NoahStalnakerSps.sql
@@ -1,8 +1,14 @@
-create procedure ShowAvgRatingatCertainTemp
-
-As
+CREATE PROCEDURE GetAverageRatingForTemperatureRange
+    @MinTemp INT,
+    @MaxTemp INT
+AS
 BEGIN
-
+    SELECT AVG(CAST(HR.Rating AS FLOAT)) AS Rating, HR.[Date], L.[Address], L.[State]
+    FROM HealthReview HR
+    JOIN LocationWeather LW ON HR.LID = LW.LID AND HR.WID = LW.WID
+    JOIN Location L ON LW.LID = L.LID
+    WHERE LW.MinTemp >= @MinTemp AND LW.MaxTemp <= @MaxTemp
+    GROUP BY HR.Rating, HR.[Date], L.[Address], L.[State];
+END;
 
   
-END
