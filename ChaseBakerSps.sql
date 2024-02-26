90478d599daa28e5d1e5eb1d978e74d5e589e2cd
cb00059 <142524817+cb00059@users.noreply.github.com>
2024-02-25 21:52:58 -0500

    Update ChaseBakerSps.sql
    Added first stored procedure
    it was mad hard

--- ChaseBakerSps.sql
+++ ChaseBakerSps.sql
@@ -1,1 +1,18 @@
-// This is the Alpha Male's Territory, do you have permission to be here?
+-- This is the Alpha Male's Territory, do you have permission to be here?
+
+--This one took forever to figure out
+CREATE PROCEDURE addAttrToTable
+	@Table varchar(30),
+	@NewAttr varchar(30),
+	@NewAttrDataType varchar(30)
+
+	AS 
+	BEGIN
+		DECLARE @stmt varchar(1000)
+		SET @stmt = 'ALTER TABLE '  + @Table  + ' ' + 'ADD '+ @NewAttr + ' ' + @NewAttrDataType
+	
+		EXECUTE (@stmt);
+		
+
+		END;
+		GO
