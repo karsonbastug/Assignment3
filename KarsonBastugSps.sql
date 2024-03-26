cb74d9d098858633effa051ac75dca8fcab93ac2
Karson Bastug <91923098+karsonbastug@users.noreply.github.com>
2024-03-26 14:54:55 -0400

    Update KarsonBastugSps.sql

--- KarsonBastugSps.sql
+++ KarsonBastugSps.sql
@@ -26,4 +26,13 @@ END;
 
 --EXEC GetCitiesByRatingAndDateRated @Rating = 3
 
-
+CREATE PROCEDURE GetCity1
+    @Location varchar(20)
+AS
+BEGIN
+    SELECT HR.[Date], HR.Rating, LW.MaxTemp, LW.MinTemp, LW.AvgTemp
+    FROM Location L
+    INNER JOIN LocationWeather LW ON L.LID = LW.LID
+    INNER JOIN HealthReview HR ON LW.LID = HR.LID AND LW.WID = HR.WID
+    WHERE L.City = @Location
+END;
