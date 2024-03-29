a8e05b45ce3da7268322374f6ec5a521b9af8703
Karson Bastug <91923098+karsonbastug@users.noreply.github.com>
2024-03-28 20:15:46 -0400

    Update KarsonBastugSps.sql

--- KarsonBastugSps.sql
+++ KarsonBastugSps.sql
@@ -1,17 +1,34 @@
 
-CREATE PROCEDURE AGetTemp
+--drop procedure GetRatingsForHighTemp
+CREATE PROCEDURE GetRatingsForHighTemp
     @MaxTemp INT
 AS
 BEGIN
-    SELECT HR.Rating, HR.[Date], L.[Address], L.[State]
+    SELECT HR.Rating, HR.[Date], HR.Comment, LW.MaxTemp
     FROM HealthReview HR
     INNER JOIN LocationWeather LW ON HR.LID = LW.LID AND HR.WID = LW.WID
     INNER JOIN Location L on LW.LID=L.LID
     WHERE LW.MaxTemp > @MaxTemp;
 END;
---drop procedure GetRatingsForHighTemp
---EXEC AGetTemp @MaxTemp = 80;
 
+--EXEC GetRatingsForHighTemp @MaxTemp = 80;
+
+CREATE PROCEDURE GetCity1
+    @Location varchar(20)
+AS
+BEGIN
+    SELECT HR.[Date], HR.Rating, LW.MaxTemp, LW.MinTemp, LW.AvgTemp
+    FROM Location L
+    INNER JOIN LocationWeather LW ON L.LID = LW.LID
+    INNER JOIN HealthReview HR ON LW.LID = HR.LID AND LW.WID = HR.WID
+    WHERE L.City = @Location
+END;
+
+
+
+
+
+--Unused 
 
 CREATE PROCEDURE GetCitiesByRatingAndDateRated
     @Rating INT
@@ -25,14 +42,3 @@ BEGIN
 END;
 
 --EXEC GetCitiesByRatingAndDateRated @Rating = 3
-
-CREATE PROCEDURE GetCity1
-    @Location varchar(20)
-AS
-BEGIN
-    SELECT HR.[Date], HR.Rating, LW.MaxTemp, LW.MinTemp, LW.AvgTemp
-    FROM Location L
-    INNER JOIN LocationWeather LW ON L.LID = LW.LID
-    INNER JOIN HealthReview HR ON LW.LID = HR.LID AND LW.WID = HR.WID
-    WHERE L.City = @Location
-END;
